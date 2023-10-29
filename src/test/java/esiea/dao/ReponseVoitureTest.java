31a835b46802375f97660a9046bd536a3f1004f5
skrafft <skrafft@et.esiea.fr>
2023-10-29 20:39:09 +0100

    Reponse Voiture test not complete

--- src/test/java/esiea/dao/ReponseVoitureTest.java
+++ src/test/java/esiea/dao/ReponseVoitureTest.java
@@ -1,4 +1,63 @@
 package esiea.dao;
 
-public class ReponseVoitureTest {
+import esiea.metier.Voiture;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReponseVoitureTest
+{
+
+    @Test
+    public void getDataTest()
+    {
+        ReponseVoiture data = new ReponseVoiture();
+        Voiture voit1 = new Voiture();
+        Voiture voit2 = new Voiture();
+        Voiture[] liste = new Voiture[] {voit1, voit2};
+
+
+        data.setData(liste);
+        assertEquals(data.getData(),liste);
+
+    }
+
+    @Test
+    public void setData2Test() {
+        ReponseVoiture reponse = new ReponseVoiture();
+        Voiture voiture = new Voiture();
+        reponse.setData(voiture, 0);
+        Voiture[] data = reponse.getData();
+        assertNotNull(data);
+        assertEquals(1, data.length);
+        assertSame(voiture, data[0]);
+    }
+
+    @Test
+    public void setDataNullTest() {
+        ReponseVoiture reponse = new ReponseVoiture();
+        Voiture voiture = new Voiture();
+
+
+    }
+
+    @Test
+    public void getVolumeTest()
+    {
+        ReponseVoiture data = new ReponseVoiture();
+        Voiture voiture = new Voiture();
+        data.setVolume(0);
+        int volume = data.getVolume();
+        assertEquals(volume,0);
+        data.setData(voiture, 0);
+
+        assertNotNull(data.getData());
+        assertEquals(1, data.getData().length);
+        Voiture[] donnes = data.getData();
+        assertEquals(voiture, donnes[0]);
+
+    }
+
+
+
 }
